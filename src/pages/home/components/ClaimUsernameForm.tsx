06fc50cf02ddeebaed8690c1a2f9ec7a7f81b698
capelaum <thecapellett@gmail.com>
2023-03-05 18:25:07 -0300

    ✨ ClaimUsername: use React Hook Form

--- src/pages/home/components/ClaimUsernameForm.tsx
+++ src/pages/home/components/ClaimUsernameForm.tsx
@@ -3,15 +3,30 @@ import {
   TextInput
 } from '@capelaum-packages/ignite-react-05-design-system-react'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { Form } from './styles'
 
+const claimUsernameFormSchema = z.object({
+  username: z.string().min(3).max(20)
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log('💥 ~ data:', data)
+  }
+
   return (
-    <Form as="form">
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
       <TextInput
         sizeValue="sm"
-        prefix="test.com.br/"
+        prefix="ignitecall.com.br/"
         placeholder="seu-usuario"
+        {...register('username')}
       />
 
       <Button size="sm" type="submit">
