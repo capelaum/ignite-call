9ebae80c6b3c28303fc52ea968a6eec340fd4cfa
capelaum <thecapellett@gmail.com>
2023-03-09 05:25:23 -0300

    ✨ Time Pickes initial styles

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,41 @@
 import { Calendar } from '@/components/Calendar'
-import { CalendarStepContainer } from './styles'
+import { useState } from 'react'
+import {
+  CalendarStepContainer,
+  TimePickerContainer,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList
+} from './styles'
 
 export function CalendarStep() {
+  const [isDaySelected, setIsDaySelected] = useState(true)
+
   return (
-    <CalendarStepContainer>
+    <CalendarStepContainer isTimePickerOpen={isDaySelected}>
       <Calendar />
+
+      {isDaySelected && (
+        <TimePickerContainer>
+          <TimePickerHeader>
+            Terça-feira <span>20 de Setembro</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>08:00</TimePickerItem>
+            <TimePickerItem>09:00</TimePickerItem>
+            <TimePickerItem>10:00</TimePickerItem>
+            <TimePickerItem>11:00</TimePickerItem>
+            <TimePickerItem>12:00</TimePickerItem>
+            <TimePickerItem>13:00</TimePickerItem>
+            <TimePickerItem>14:00</TimePickerItem>
+            <TimePickerItem>15:00</TimePickerItem>
+            <TimePickerItem>16:00</TimePickerItem>
+            <TimePickerItem>17:00</TimePickerItem>
+            <TimePickerItem>18:00</TimePickerItem>
+          </TimePickerList>
+        </TimePickerContainer>
+      )}
     </CalendarStepContainer>
   )
 }
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
@@ -1,6 +1,7 @@
 import {
   Box,
-  styled
+  styled,
+  Text
 } from '@capelaum-packages/ignite-react-05-design-system-react'
 
 export const CalendarStepContainer = styled(Box, {
@@ -9,10 +10,114 @@ export const CalendarStepContainer = styled(Box, {
   display: 'grid',
   position: 'relative',
 
-  width: 540,
-  gridTemplateColumns: '1fr',
+  variants: {
+    isTimePickerOpen: {
+      true: {
+        gridTemplateColumns: '1fr 280px',
+
+        '@bp1': {
+          gridTemplateColumns: '1fr'
+        }
+      },
+      false: {
+        width: 540,
+        gridTemplateColumns: '1fr'
+      }
+    }
+  },
 
   '@bp2': {
     width: '100%'
+  }
+})
+
+export const TimePickerContainer = styled('div', {
+  borderLeft: '1px solid $gray600',
+  padding: '$6 $6 0',
+  overflowY: 'scroll',
+
+  position: 'absolute',
+  top: 0,
+  bottom: 0,
+  right: 0,
+  width: 280,
+
+  // set scrollbar styles
+  '&::-webkit-scrollbar': {
+    width: 8,
+    height: 8,
+    borderRadius: '$sm'
+  },
+
+  '&::-webkit-scrollbar-track': {
+    background: '$gray600',
+    borderRadius: '$sm',
+    border: '1px solid $gray600'
+  },
+
+  '&::-webkit-scrollbar-thumb': {
+    background: '$gray500',
+    borderRadius: '$sm',
+    border: '1px solid $gray500',
+
+    transition: '$fast',
+
+    '&:hover': {
+      background: '$gray400'
+    },
+
+    '&:active': {
+      background: '$gray400'
+    }
+  }
+})
+
+export const TimePickerHeader = styled(Text, {
+  fontWeight: '$medium',
+
+  span: {
+    color: '$gray200'
+  }
+})
+
+export const TimePickerList = styled('div', {
+  marginTop: '$3',
+  display: 'grid',
+  gridTemplateColumns: '1fr',
+  gap: '$2',
+
+  '@bp1': {
+    gridTemplateColumns: '2fr'
+  }
+})
+
+export const TimePickerItem = styled('button', {
+  backgroundColor: '$gray600',
+  padding: '$2 0',
+  color: '$gray100',
+  borderRadius: '$sm',
+  fontSize: '$sm',
+  lineHeight: '$base',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  '&:last-child': {
+    marginBottom: '$6'
+  },
+
+  '&:disabled': {
+    background: 'none',
+    cursor: 'default',
+    opacity: 0.4
+  },
+
+  '&:not(:disabled):hover': {
+    background: '$gray500'
+  },
+
+  '&:focus': {
+    boxShadow: '0 0 0 2px $colors$gray100'
   }
 })
